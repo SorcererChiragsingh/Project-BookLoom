860a7bb82053c2132a289cee52077ed11824da10
Chirag singh <chiragsingh8926@gmail.com>
2025-02-09 20:01:11 +0530

    remove database connection

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,4 @@ export const PORT = 5555;
 
 
 export const mongoDBURL = 
-'chiragsingh8926:<db_password>@bookstore-mern-stack.wh11n.mongodb.net/?retryWrites=true&w=majority&appName=BookStore-Mern-Stack'
+''
