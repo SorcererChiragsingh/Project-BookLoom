5334356b0ab6aff7489830016570c31a5d114125
Chirag singh <chiragsingh8926@gmail.com>
2025-02-18 02:02:24 +0530

    Add monodb and mongoose to node.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { PORT , mongoDBURL } from './config.js';
-
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -9,7 +9,18 @@ app.get(`/`, (request , response) => {
     return response.status(234).send ('Hello World');
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
 
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('App (MongoDB) connected to database successfully');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    
+    });
+})
+
+.catch((error) => {
+    console.log(error);
 });
