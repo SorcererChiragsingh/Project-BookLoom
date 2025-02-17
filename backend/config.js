8ae2f393fd1c46f06f8e7b7a727737f9f85bb9c8
Chirag singh <chiragsingh8926@gmail.com>
2025-02-17 21:38:09 +0530

    Config file changes

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,7 @@ export const PORT = 5555;
 
 
 export const mongoDBURL = 
-''
+'mongodb+srv://chiragsingh8926:Zaf825mhTjZxx2DX@bookstore-mern-stack.uv67o.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-Mern-Stack'
+
+// user id:- chiragsingh8926
+// password:- Zaf825mhTjZxx2DX
