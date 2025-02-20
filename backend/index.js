ff832fbdea1ff2cccac057d3fbc83ec5d321ac38
Chirag singh <chiragsingh8926@gmail.com>
2025-02-20 18:44:30 +0530

    Route for book

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ import {Book} from './models/bookModel.js';
 
 const app = express();
 
+// Middleware for passing request body
+app.use(express.json()); 
+
+
 app.get(`/`, (request , response) => {
     console.log(request)
     return response.status(234).send ('Hello World');
@@ -39,7 +43,48 @@ app.post('/books', async (request, response) => {
     }
   });
 
-  
+// route for get all books from database
+app.get('/books', async (request, response) => {
+    try {
+          const books = await Book.find({});
+          return response.status(200).json({ 
+            count: books.length,
+            data: books,
+
+          });
+
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({message: error.message});
+    }
+  });
+
+// route for get all books from database by id
+app.get('/books/:id', async (request, response) => {
+  try {
+        const{id} = request.params;
+
+
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({message: error.message});
+  }
+});
+
+// Route for a Book
+app.put('/books/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+  }
+  catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }});
+
 mongoose
 .connect(mongoDBURL)
 .then(() => {
