f44bf1ac74135d80f66a35248784cbd6597bc55e
Chirag singh <chiragsingh8926@gmail.com>
2025-02-23 01:43:06 +0530

    CORS policy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,19 @@ app.get(`/`, (request , response) => {
     return response.status(234).send ('Hello World');
 });
 
-// 
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+// Accesing routes in index file
 app.use('/books', booksRoute);
 
 // Mongoose Database connection  
