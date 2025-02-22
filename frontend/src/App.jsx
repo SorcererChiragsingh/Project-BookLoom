a49b68b5385182036b4063e756fa7871d019226d
Chirag singh <chiragsingh8926@gmail.com>
2025-02-23 02:30:07 +0530

    working on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,13 +9,13 @@ import DeleteBook from './pages/DeleteBook';
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBook />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
-    </Routes>
-  );
+     <Route path='/' element={<Home />} />
+     <Route path='/books/create' element={<CreateBook />} />
+     <Route path='/books/details/:id' element={<ShowBook />} />
+     <Route path='/books/edit/:id' element={<EditBook />} />
+     <Route path='/books/delete/:id' element={<DeleteBook />} />
+   </Routes>
+ );
 };
 
 export default App;
